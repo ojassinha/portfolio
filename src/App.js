8dc8bb90dc08704068485a83caaf1905d9f70f9b
sinhaojas <ojassinha90@gmail.com>
2018-01-03 01:59:22 +0530

    test commit -2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          I am an awesome project 
+          I am not an awesome project 
         </p>
       </div>
     );
