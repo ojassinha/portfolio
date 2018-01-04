13e1ad20a3f214ef1e0d5846f420b6c3afff49b6
sinhaojas <ojassinha90@gmail.com>
2018-01-05 01:31:41 +0530

    added material-ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+    
+          <h2 className="App-title">This page is under construction </h2>
         </header>
+        <AppBar title = "Articles" iconClassNameRight="uidocs-icon-navigation-expand-more">
+        </AppBar>
+        
         <p className="App-intro">
           I am not an awesome project 
         </p>
+        <RaisedButton label="Default">
+        </RaisedButton>
+        
       </div>
+      </MuiThemeProvider>
     );
   }
 }
